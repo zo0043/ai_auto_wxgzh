6626f20caf8fc6b9d29c904a8f03b7df4c1c4409
iniwap <iniwaper@gmail.com>
2025-04-16 13:48:14 +0800

    增加处理降低token消耗

--- src/ai_auto_wxgzh/tools/custom_tool.py
+++ src/ai_auto_wxgzh/tools/custom_tool.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 import os
 import glob
 import random
+import sys
 
 from tools.wx_publisher import WeixinPublisher
 from utils import utils
@@ -28,9 +29,11 @@ class ReadTemplateTool(BaseTool):
         template_files_abs = glob.glob(os.path.join(template_dir_abs, "*.html"))
 
         if not template_files_abs:
-            raise FileNotFoundError(
-                f"在目录 '{template_dir_abs}' 中未找到任何 HTML 模板文件。请确保模板文件存在。"
+            print(
+                f"在目录 '{template_dir_abs}' 中未找到任何模板文件。如果没有模板请将config.yaml中的use_template设置为false"
             )
+            # 出现这种错误无法继续，立即终止程序，防止继续消耗Tokens（不终止CrewAI可能会重试）
+            sys.exit(1)
 
         selected_template_file = random.choice(template_files_abs)
         with open(selected_template_file, "r", encoding="utf-8") as file:
@@ -66,35 +69,31 @@ class PublisherTool(BaseTool):
         # 自定义工具接收上一个task数据有很大随机性，这里只能从保存的文件读取数据
         tmp_article = os.path.join(utils.get_current_dir(), "tmp_article.html")
 
-        with open(tmp_article, "r", encoding="utf-8") as file:
-            content = file.read()
+        try:
+            with open(tmp_article, "r", encoding="utf-8") as file:
+                content = file.read()
+        except Exception as e:
+            print(str(e))
+            return "读取tmp_article.html失败，无法发布文章！"
 
         # 提取审核报告中修改后的文章
         article = utils.extract_modified_article(content)
 
-        if article is None:
-            # 如果没有找到 html，则返回错误消息
-            return "未找到修改后的文章。"
-
         # 发布到微信公众号
-        try:
-            article = self.pub2wx(
-                article,
-                appid,
-                appsecret,
-                author,
-                img_api_type,
-                img_api_key,
-                img_api_model,
-            )
-        except Exception as e:
-            print(str(e))
-
+        result, article = self.pub2wx(
+            article,
+            appid,
+            appsecret,
+            author,
+            img_api_type,
+            img_api_key,
+            img_api_model,
+        )
         # 保存为 final_article.html
         with open("final_article.html", "w", encoding="utf-8") as file:
             file.write(article)
 
-        return "文章已成功保存并发布。"
+        return result
 
     def pub2wx(
         self,
@@ -109,7 +108,7 @@ class PublisherTool(BaseTool):
         try:
             title, digest = utils.extract_html(article)
         except Exception as e:
-            print(f"PublisherTool 执行出错: {e}")
+            return f"从文章中提取标题、摘要信息出错: {e}", article
 
         publisher = WeixinPublisher(
             appid, appsecret, author, img_api_type, img_api_key, img_api_model
@@ -119,36 +118,54 @@ class PublisherTool(BaseTool):
             "主题：" + title.split("|")[-1] + "，内容：" + digest,
             "900*384",
         )
+        if image_url is None:
+            print("生成图片出错，使用默认图片")
 
         # 封面图片
         media_id, _ = publisher.upload_image(image_url)
+        if media_id is None:
+            return "上传封面图片出错，无法发布文章", article
 
         # 这里需要将文章中的图片url替换为上传到微信返回的图片url
-        image_urls = utils.extract_image_urls(article)
-        for image_url in image_urls:
-            local_filename = utils.download_and_save_image(
-                image_url,
-                utils.get_current_dir("image"),
-            )
-            if local_filename:
-                _, url = publisher.upload_image(local_filename)
-                article = article.replace(image_url, url)
+        try:
+            image_urls = utils.extract_image_urls(article)
+            for image_url in image_urls:
+                local_filename = utils.download_and_save_image(
+                    image_url,
+                    utils.get_current_dir("image"),
+                )
+                if local_filename:
+                    _, url = publisher.upload_image(local_filename)
+                    article = article.replace(image_url, url)
+        except Exception as e:
+            print(f"上传配图出错，影响阅读，可继续发布文章:{e}")
 
         add_draft_result = publisher.add_draft(article, title, digest, media_id)
+        if add_draft_result is None:
+            # 添加草稿失败，不再继续执行
+            return "上传草稿失败，无法发布文章", article
 
         publish_result = publisher.publish(
             add_draft_result.publishId
         )  # 可以利用返回值，做重试等处理
+        if publish_result is None:
+            return "发布草稿失败，无法继续发布文章", article
 
         article_url = publisher.poll_article_url(publish_result.publishId)
         if article_url is not None:
-            # 该接口需要认证
-            publisher.create_menu(article_url)
+            # 该接口需要认证，将文章添加到菜单中去，用户可以通过菜单“最新文章”获取到
+            _ = publisher.create_menu(article_url)
         else:
-            print("无法获取到文章URL")
+            print("无法获取到文章URL")  # 这里无所谓，不影响后面
 
+        # 只有下面执行成功，文章才会显示到公众号列表，否则只能通过后台复制链接分享访问
         # 通过群发使得文章显示到公众号列表 ——> 该接口需要认证
         media_id = publisher.media_uploadnews(article, title, digest, media_id)
-        publisher.message_mass_sendall(media_id)
+        if media_id is None:
+            return "上传图文素材失败，无法显示到公众号文章列表", article
 
-        return article
+        sndall_ret = publisher.message_mass_sendall(media_id)
+        if sndall_ret is None:
+            return "无法将文章群发给用户，无法显示到公众号文章列表", article
+
+        return "成功发布文章到微信公众号", article
